bd2c12a71bcf982f40bde2b2614d7ec485664758
mlanca-c <marialsottomayor@gmail.com>
2022-10-28 15:53:49 +0100

    Fixed php index bug

--- srcs/requirements/wordpress/tools/wordpress-setup.sh
+++ srcs/requirements/wordpress/tools/wordpress-setup.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-while ! nc -z "mariadb" "3306"; do
-	sleep 1;
-done
-
-# wp-config.php file
-wp config create \
-		--dbname=${DB_NAME} \
-		--dbuser=${DB_USER} \
-		--dbpass=${DB_PASSWORD} \
-		--dbhost=${DB_HOST} \
-		--allow-root
-
-# create administrator
-chmod 644 wp-config.php
-wp core install \
-	--url=${WP_URL} \
-	--title=${WP_TITLE} \
-	--admin_name=${WP_ADMIN} \
-	--admin_password=${WP_ADMIN_PASS} \
-	--admin_email=${WP_ADMIN_MAIL} \
-	--allow-root
-
-# Create user
-wp user create ${WP_USER} ${WP_USER_MAIL} \
-	--user_pass=${WP_USER_PASS} \
-	--role=editor \
-	--allow-root
-
-service php7.3-fpm start;
-service php7.3-fpm stop;
-php-fpm7.3 -F -R;
+# while ! nc -z "mariadb" "3306"; do
+# 	sleep 1;
+# done
+#
+# if [ -f "/var/www/wordpress/wp-config.php" ]; then
+# 	echo "${WP_URL} already created"
+# else
+# 	cp -f /tmp/wp-config.php /var/www/wordpress/
+# 	chmod 644 /var/www/wordpress/wp-config.php;
+#
+# 	# create administrator
+# 	wp core install \
+# 		--url="${WP_URL}" \
+# 		--title="${WP_TITLE}" \
+# 		--admin_name="${WP_ADMIN}" \
+# 		--admin_email="${WP_ADMIN_MAIL}" \
+# 		--admin_password="${WP_ADMIN_PASS}" --allow-root;
+#
+# 	# Create user
+# 	wp user create \
+# 		"${WP_USER}" \
+# 		"${WP_USER_MAIL}" \
+# 		--user_pass="${WP_USER_PASS}" \
+# 		--role=editor --allow-root;
+# fi
+#
+# service php7.3-fpm start;
+# service php7.3-fpm stop;
+#
+# php-fpm7.3 -F -R;
